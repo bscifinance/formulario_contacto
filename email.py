608767b8c1f059558f8a11d509b2db88d2811ac7
Pablo Ramirez <105121715+bscifinance@users.noreply.github.com>
2023-03-15 10:33:34 -0300

    Update email.py

--- email.py
+++ email.py
@@ -14,6 +14,22 @@ formulario_contacto = """
      <button type="submit">Send</button>
 </form>
 """
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            footer:after {
+                content:'bscifinance'; 
+                visibility: visible;
+                display: block;
+                position: relative;
+                #background-color: red;
+                padding: 5px;
+                top: 2px;
+            }
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown(formulario_contacto, unsafe_allow_html=True)
 
